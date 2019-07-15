89c4a72b90dd6e35e98f8c59aae158d4a0472be3
Depish <reactor.game@gmail.com>
2019-07-15 15:52:15 +0300

    fix script

--- scripts/linux/install_SFML_dependencies.sh
+++ scripts/linux/install_SFML_dependencies.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-apt-get install libx11-dev -y
-apt-get install libxcb1-dev -y
-apt-get install libx11-xcb-dev -y
-apt-get install libxcb-randr0-dev -y
-apt-get install libxcb-image0-dev -y
-apt-get install libgl1-mesa-dev -y
-apt-get install libudev-dev -y
+apt-get install libx11-dev libxcb1-dev libx11-xcb-dev libxcb-randr0-dev libxcb-image0-dev libgl1-mesa-dev libudev-dev
 
-apt-get install libfreetype6-dev -y
-apt-get install libjpeg-dev -y
+apt-get install libfreetype6-dev libjpeg-dev
 
-apt-get install libopenal-dev -y
-apt-get install libflac-dev -y
-apt-get install libvorbis-dev -y
+apt-get install libopenal-dev libflac-dev  libvorbis-dev 
