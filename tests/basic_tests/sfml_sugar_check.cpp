5febd0e82cb2a360d15b68ee0adb39f1e6f809f9
Depish <reactor.game@gmail.com>
2019-07-20 16:33:07 +0300

    delete sfml sugar

--- tests/basic_tests/sfml_sugar_check.cpp
+++
@@ -1,8 +0,0 @@
-
-#include "gtest/gtest.h"
-
-#include "TMXL/SFMLSugar.h"
-
-TEST(tmxl_sfml_sugar_to_const_char_check, test_eq) {
-    ASSERT_STREQ(TMXL::sfStringToChar({"Test string"}), "Test string");
-}
