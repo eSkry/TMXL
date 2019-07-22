b86f7ddb8c73aa2aa0258a061b715d8c46652032
eSkry <reactor.game@gmail.com>
2019-07-22 22:14:56 +0300

    add interface for encoders

---
+++ TMXL/include/TMXL/Encodings/IEncoder.h
@@ -0,0 +1,28 @@
+#ifndef TMXL_IENCODER_H
+#define TMXL_IENCODER_H
+
+#include <ostream>
+#include <string>
+
+namespace TMXL {
+
+    /**
+     * @details Base class for encoders
+     */
+    class IEncoder {
+    public:
+
+        virtual std::string encode(const std::string& data) = 0;
+        virtual std::string decode(const std::string& data) = 0;
+
+        virtual void encode(const std::string& data, std::ostream& os) = 0;
+        virtual void decode(const std::string& data, std::ostream& os) = 0;
+
+        virtual void encode(const std::string& data, std::string& out) = 0;
+        virtual void decode(const std::string& data, std::string& out) = 0;
+        
+    };
+
+}
+
+#endif //TMXL_IENCODER_H
