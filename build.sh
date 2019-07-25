a56d83bbf1ddb1b5ea7055c136146a513786feb9
Depish <reactor.game@gmail.com>
2019-07-25 10:50:12 +0300

    add test to build

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 rm -r ./build
 mkdir build && cd ./build
-cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON
+cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DBUILD_TMXL_TEST_ONLY=ON
 cmake --build .
 cd ..
