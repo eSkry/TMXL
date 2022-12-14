dfd4815f1a74e164c19e5e578c46bd4c4d2dd3ea
Depish <depish.eskry@yandex.ru>
2022-12-14 08:40:21 +0300

    Добавил pugixml в зависимости conan

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 rm -r ./build
 mkdir build && cd ./build
-cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DBUILD_TMXL_TEST_ONLY=ON
+conan install .. --build=missing
+cmake -G "Unix Makefiles" .. -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=ON -DBUILD_TMXL_TEST_ONLY=OFF
 cmake --build .
 cd ..
